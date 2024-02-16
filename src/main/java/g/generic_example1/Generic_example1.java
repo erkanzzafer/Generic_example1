4474234aa0873f7ccb60e33a3ba2519ec45d4b14
erkanzzafer <zzafererkann@gmail.com>
2024-02-16 10:42:57 +0300

    first commit

---
+++ src/main/java/g/generic_example1/Generic_example1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package g.generic_example1;
+
+/**
+ *
+ * @author zafer
+ */
+public class Generic_example1 {
+
+    public static void main(String[] args) {
+        MyList<Customers> sehirler = new MyList<>();
+        sehirler.add(new Customers());
+        sehirler.add(new Customers());
+    }
+}
---
+++ src/main/java/g/generic_example1/MyList.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package g.generic_example1;
+
+/**
+ *
+ * @author zafer
+ */
+public class MyList<T> {
+
+    public void add(T value) {
+
+        
+    }
+
+    public void remove(T value) {
+
+    }
+
+}
